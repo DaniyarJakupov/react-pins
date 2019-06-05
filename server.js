5d317f31eac19d0d0791b4022531512e8f028898
Daniyar Jakupov <ddr199305@gmail.com>
2019-06-05 13:07:23 +0600

    Connect mongo db

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
