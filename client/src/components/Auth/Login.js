94bc5e48208e9a0305906f32e855e151038fb243
Daniyar Jakupov <ddr199305@gmail.com>
2019-06-07 12:12:57 +0600

    Call ME Query

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
+import { GraphQLClient } from "graphql-request";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+  {
+    me {
+      name
+      email
+      picture
+      _id
+    }
+  } 
+`;
+
 const Login = ({ classes }) => {
-  const onSuccess = googleUser => {
+  const onSuccess = async googleUser => {
     const idToken = googleUser.getAuthResponse().id_token;
-    console.log(idToken);
+    const client = new GraphQLClient("http://localhost:4000/graphql", {
+      headers: { authorization: idToken }
+    });
+    let response = await client.request(ME_QUERY);
+    console.log({ response });
   };
 
   return <GoogleLogin clientId="" onSuccess={onSuccess} isSignedIn={true} />;
