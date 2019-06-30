8263a066667262b60692a5e0d7fa3a136bed5645
Daniyar Jakupov <ddr199305@gmail.com>
2019-06-30 11:50:58 +0600

    Update context state when logged in

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GoogleLogin } from "react-google-login";
 import { GraphQLClient } from "graphql-request";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
-const ME_QUERY = `
-  {
-    me {
-      name
-      email
-      picture
-      _id
-    }
-  } 
-`;
+import AppContext from "../../context";
+import { ME_QUERY } from "../../graphql/queries";
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(AppContext);
+
   const onSuccess = async googleUser => {
-    const idToken = googleUser.getAuthResponse().id_token;
-    const client = new GraphQLClient("http://localhost:4000/graphql", {
-      headers: { authorization: idToken }
-    });
-    let response = await client.request(ME_QUERY);
-    console.log({ response });
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient("http://localhost:4000/graphql", {
+        headers: { authorization: idToken }
+      });
+      let { me } = await client.request(ME_QUERY);
+      dispatch({ type: "LOGIN_USER", payload: me });
+    } catch (e) {
+      onFailure(e);
+    }
+  };
+
+  const onFailure = err => {
+    console.error(`Error logging in, ${err}`);
   };
 
   return (
-    <GoogleLogin
-      clientId="539730108119-3ku7clq0lpq0tneolcugh7jrp266usi3.apps.googleusercontent.com"
-      onSuccess={onSuccess}
-      isSignedIn={true}
-    />
+    <div className={classes.root}>
+      <Typography component="h1" variant="h3" gutterBottom noWrap style={{ color: "rgb(66, 133, 244)" }}>
+        Welcome to ReactPins!
+      </Typography>
+      <GoogleLogin
+        clientId="539730108119-3ku7clq0lpq0tneolcugh7jrp266usi3.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        isSignedIn={true}
+        onFailure={onFailure}
+        theme="dark"
+      />
+    </div>
   );
 };
 
