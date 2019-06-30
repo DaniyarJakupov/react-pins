4c9524cda4ca544bd28db8ec30db480c16655948
Daniyar Jakupov <ddr199305@gmail.com>
2019-06-30 14:19:44 +0600

    Add sign out case

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -4,6 +4,8 @@ export default function reducer(state, action) {
       return { ...state, currentUser: action.payload };
     case "UPDATE_IS_AUTH":
       return { ...state, isAuth: action.payload };
+    case "SIGN_OUT_USER":
+      return { ...state, currentUser: null, isAuth: false };
     default:
       return state;
   }
