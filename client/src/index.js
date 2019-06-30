41a440770194d5677a592797b53c0920fb12355d
Daniyar Jakupov <ddr199305@gmail.com>
2019-06-30 11:50:22 +0600

    Provide state & dispatch via context

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import App from "./pages/App";
 import Splash from "./pages/Splash";
+import AppContext from "./context";
+import reducer from "./reducer";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import * as serviceWorker from "./serviceWorker";
 
 const Root = () => {
+  const initState = useContext(AppContext);
+  const [state, dispatch] = useReducer(reducer, initState);
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/login" component={Splash} />
-      </Switch>
+      <AppContext.Provider value={{ state, dispatch }}>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Splash} />
+        </Switch>
+      </AppContext.Provider>
     </Router>
   );
 };
