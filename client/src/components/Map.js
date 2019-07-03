7bd142a03f62a6d5afcffcf23758b71088c1d9ca
Daniyar Jakupov <ddr199305@gmail.com>
2019-07-03 23:17:56 +0600

    Comment out imports

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import ReactMapGl, { NavigationControl, Marker } from "react-map-gl";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+// import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
+// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
 import PinIcon from "./PinIcon";
 import Blog from "./Blog";
