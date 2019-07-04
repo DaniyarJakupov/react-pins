b984bccc638168667152dbed764e81bc0e43ff79
Daniyar Jakupov <ddr199305@gmail.com>
2019-07-04 23:22:18 +0600

    Configure apollo client for subscriptions

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
 import App from "./pages/App";
 import Splash from "./pages/Splash";
 import AppContext from "./context";
 import reducer from "./reducer";
 import PrivateRoute from "./hoc/PrivateRoute";
+
+/* Configure Apollo */
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true
+  }
+});
+const apolloClient = new ApolloClient({
+  link: wsLink,
+  cache: new InMemoryCache()
+});
 
 const Root = () => {
   const initState = useContext(AppContext);
@@ -15,12 +32,14 @@ const Root = () => {
 
   return (
     <Router>
-      <AppContext.Provider value={{ state, dispatch }}>
-        <Switch>
-          <PrivateRoute exact path="/" component={App} />
-          <Route path="/login" component={Splash} />
-        </Switch>
-      </AppContext.Provider>
+      <ApolloProvider client={apolloClient}>
+        <AppContext.Provider value={{ state, dispatch }}>
+          <Switch>
+            <PrivateRoute exact path="/" component={App} />
+            <Route path="/login" component={Splash} />
+          </Switch>
+        </AppContext.Provider>
+      </ApolloProvider>
     </Router>
   );
 };
