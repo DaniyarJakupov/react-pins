efe09c7378d4b09798d5b4dd561dc3ac5ad001ce
Daniyar Jakupov <ddr199305@gmail.com>
2019-07-04 11:08:52 +0600

    Add SET_PIN reducer

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -7,7 +7,7 @@ export default function reducer(state, action) {
     case "SIGN_OUT_USER":
       return { ...state, currentUser: null, isAuth: false };
     case "CREATE_DRAFT":
-      return { ...state, draft: { latitude: 0, longitude: 0 } };
+      return { ...state, currentPin: null, draft: { latitude: 0, longitude: 0 } };
     case "UPDATE_DRAFT_LOCATION":
       return { ...state, draft: action.payload };
     case "REMOVE_DRAFT":
@@ -18,6 +18,8 @@ export default function reducer(state, action) {
       const newPin = action.payload;
       const prevPins = state.pins.filter(pin => pin._id !== newPin._id);
       return { ...state, pins: [...prevPins, newPin] };
+    case "SET_PIN":
+      return { ...state, currentPin: action.payload, draft: null };
     default:
       return state;
   }
